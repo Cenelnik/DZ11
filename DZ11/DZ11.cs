c7ba70b85bb094251e61fa5da765f43e36611671
cenelnik@yandex.ru <cenelnik@yandex.ru>
2024-03-11 15:35:19 +0300

    Добавьте файлы проекта.

---
+++ DZ11/DZ11.cs
@@ -0,0 +1,17 @@
+﻿namespace DZ11
+{
+    /// <summary>
+    /// 11я домашка по OUTUS. OtusDictionary
+    /// </summary>
+    class DZ11
+    {
+        /// <summary>
+        /// Тут есть опиисание метода по словаря
+        /// </summary>
+        /// <param name="args"></param>
+        public static async Task Main(string[] args)
+        {
+
+        }
+    }
+}
