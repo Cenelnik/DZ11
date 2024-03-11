1efadcdca54e1645616444203c6757f344459893
cenelnik@yandex.ru <cenelnik@yandex.ru>
2024-03-11 15:38:57 +0300

    Создание dev ветки для тестов

--- DZ11/DZ11.cs
+++ DZ11/DZ11.cs
@@ -1,7 +1,7 @@
 ﻿namespace DZ11
 {
     /// <summary>
-    /// 11я домашка по OUTUS. OtusDictionary
+    /// 11я домашка по OUTUS. OtusDictionary. dev-barnch
     /// </summary>
     class DZ11
     {
